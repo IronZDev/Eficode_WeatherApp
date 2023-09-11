f411ef559ddd816af3fafe3d26073f75f28d6353
IronZaphilith <maciek_st@o2.pl>
2023-09-11 17:20:31 +0200

    Set up ESLint + formatted all files

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,8 +6,9 @@ const fetch = require('node-fetch');
 const cors = require('kcors');
 
 const appId = process.env.APPID || '';
-const mapURI = process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
-const targetCity = process.env.TARGET_CITY || "Helsinki,fi";
+const mapURI =
+  process.env.MAP_ENDPOINT || 'http://api.openweathermap.org/data/2.5';
+const targetCity = process.env.TARGET_CITY || 'Helsinki,fi';
 
 const port = process.env.PORT || 9000;
 
@@ -19,10 +20,10 @@ const fetchWeather = async () => {
   const endpoint = `${mapURI}/weather?q=${targetCity}&appid=${appId}&`;
   const response = await fetch(endpoint);
 
-  return response ? response.json() : {}
+  return response ? response.json() : {};
 };
 
-router.get('/api/weather', async ctx => {
+router.get('/api/weather', async (ctx) => {
   const weatherData = await fetchWeather();
 
   ctx.type = 'application/json; charset=utf-8';
