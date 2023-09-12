212a5cec6dffc95434e2cd74b3647945630a6199
IronZaphilith <maciek_st@o2.pl>
2023-09-12 19:28:38 +0200

    Updated backend to handle query params

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,8 @@
 const debug = require('debug')('weathermap');
+// Read env vars from .env file in dev mode
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({path: "./.env.dev"});
+}
 
 const Koa = require('koa');
 const router = require('koa-router')();
@@ -8,7 +12,6 @@ const cors = require('kcors');
 const appId = process.env.APPID || '';
 const mapURI =
   process.env.MAP_ENDPOINT || 'http://api.openweathermap.org/data/2.5';
-const targetCity = process.env.TARGET_CITY || 'Helsinki,fi';
 
 const port = process.env.PORT || 9000;
 
@@ -16,18 +19,22 @@ const app = new Koa();
 
 app.use(cors());
 
-const fetchWeather = async () => {
-  const endpoint = `${mapURI}/weather?q=${targetCity}&appid=${appId}&`;
+const fetchWeather = async (latitude, longitude, dataPointsToFetch) => {
+  const endpoint = `${mapURI}/forecast?lat=${latitude}&lon=${longitude}&units=metric&cnt=${dataPointsToFetch}&appid=${appId}`;
   const response = await fetch(endpoint);
 
   return response ? response.json() : {};
 };
 
 router.get('/api/weather', async (ctx) => {
-  const weatherData = await fetchWeather();
+  const queryParams = ctx.request.query;
+  const latitude = queryParams?.latitude || 51.7;
+  const longitude = queryParams?.longitude || 19.4;
+  const dataPointsToFetch = queryParams?.dataPointsToFetch || 40; // In free plan of OpenWeatherMap the highest value 40 (5 days, every 3 hours)
+  const weatherData = await fetchWeather(latitude, longitude, dataPointsToFetch);
 
   ctx.type = 'application/json; charset=utf-8';
-  ctx.body = weatherData.weather ? weatherData.weather[0] : {};
+  ctx.body = weatherData;
 });
 
 app.use(router.routes());
